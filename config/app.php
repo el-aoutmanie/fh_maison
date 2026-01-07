ed515514933a109de0cb7f912f203d5d512b9772
Mr. AZZI Aoutmane <othman.azzi04@gmail.com>
2026-01-07 11:51:14 +0100

    Fix syntax error in config/app.php

--- config/app.php
+++ config/app.php
@@ -92,18 +92,6 @@ return [
     */
 
     'fallback_locale' => 'fr',
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
-    |
-    */
-
-    'locale' => env('APP_LOCALE', 'en'),
-
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
